0c205ff65334704143c0df32064e1f2a5f867241
kevinstrax <847967921@qq.com>
2025-04-07 03:11:13 +0800

    Updates

---
+++ assets/2025_03_30-DPMjYgCk.js
@@ -0,0 +1,17 @@
+const n="【歌枠｜KARAOKE】ギャップで風邪引かせるかも…！？見た目から歌声想像できる？【#パレプロ #香鳴ハノン】",t="香鳴ハノン",s="XJh-6tPbAEg",o="2025-03-30",e=`0:02:58 はのは〜！
+
+0:09:57 01. ふでペン ～ボールペン～ / 放課後ティータイム
+0:16:22 02. Don't say "lazy" / 桜高軽音部
+0:25:14 03. わたしの一番かわいいところ / FRUITS ZIPPER
+0:31:34 04. アンビバレント / 欅坂46
+0:39:12 05. 世界一可愛い私 / 藤田ことね
+
+0:45:16 06. Luna say maybe / 月村手毬
+0:51:37 07. 粛聖!! ロリ神レクイエム☆ / しぐれうい
+1:03:56 08. ダンスホール / Mrs. GREEN APPLE
+1:09:19 09. ダダダダ天使 / ナナヲアカリ
+1:18:10 10. Q&A リサイタル!  / 戸松遥
+
+1:24:08 スパチャ読み
+1:42:25 はのは〜！
+1:42:48 END / Cパート`,i={video_title:n,video_artist:t,video_id:s,video_publish_date_str:o,song_timeline:e};export{i as default,e as song_timeline,t as video_artist,s as video_id,o as video_publish_date_str,n as video_title};
---
+++ assets/2025_04_05-BwhLiaOW.js
@@ -0,0 +1,24 @@
+const n="【歌枠｜KARAOKE】ロボソン縛り歌枠🤖✨キテレツ大百科・ガンダム・マクロスなど！【#パレプロ #香鳴ハノン】",t="香鳴ハノン",s="8bMDoq_i5ss",o="2025-04-05",i=`0:01:49 はのは〜！
+
+0:03:19 01. 鉄腕アトム / 上高田少年合唱団
+0:07:50 02. お料理行進曲 / YUKA
+0:11:29 03. すいみん不足 / CHICKS
+0:16:10 04. 夢をかなえてドラえもん / mao
+0:20:36 05. YUME日和 / 島谷ひとみ
+
+0:26:18 06. ドラえもんのうた / 大杉久美子
+0:30:38 07. 檄！帝国華撃団 / 真宮寺さくら（横山智佐）＆帝国歌劇団
+0:34:42 08. trust you / 伊藤由奈
+0:44:56 09. 蒼のエーテル / ランカ・リー＝中島愛
+0:49:03 10. ノーザンクロス / シェリル・ノーム starring May'n
+
+0:54:21 11. いけないボーダーライン / ワルキューレ
+0:59:38 12. 空色デイズ / 中川翔子
+1:11:24 13. 創聖のアクエリオン / AKINO
+
+1:17:11 スパチャ読み
+
+1:20:05 14. ダイアモンド クレバス / シェリル・ノーム starring May'n
+1:22:23 15. ライオン / シェリル・ノーム starring May'n,ランカ・リー=中島愛
+
+1:27:17 はのは〜！`,a={video_title:n,video_artist:t,video_id:s,video_publish_date_str:o,song_timeline:i};export{a as default,i as song_timeline,t as video_artist,s as video_id,o as video_publish_date_str,n as video_title};
