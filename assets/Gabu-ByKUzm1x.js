4ce4ff69aef557768dfa3f7494558b159d74b6e9
kevinstrax <847967921@qq.com>
2025-04-12 23:50:21 +0800

    Updates

---
+++ assets/Gabu-ByKUzm1x.js
@@ -0,0 +1,1 @@
+import{S as e,H as o}from"./HelloWorld-_gMDrbuf.js";import{d as r,c as a,u as t,o as n}from"./index-BmCozGkn.js";const p=r({__name:"Gabu",setup(s){return(u,c)=>(n(),a(o,{vtuber:t(e)},null,8,["vtuber"]))}});export{p as default};
