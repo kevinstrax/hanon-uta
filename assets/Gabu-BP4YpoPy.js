12a420ffc3b589a54aaaf7356b9f5c5acc43a8c4
kevinstrax <847967921@qq.com>
2025-04-13 00:13:54 +0800

    Updates

---
+++ assets/Gabu-BP4YpoPy.js
@@ -0,0 +1,1 @@
+import{S as e,H as o}from"./HelloWorld-D4BfR5sO.js";import{d as r,c as a,u as t,o as n}from"./index-CrP69OJV.js";const p=r({__name:"Gabu",setup(s){return(u,c)=>(n(),a(o,{vtuber:t(e)},null,8,["vtuber"]))}});export{p as default};
