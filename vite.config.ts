c2c01f94e165d10b714c7508241004d79b280daf
kevinstrax <847967921@qq.com>
2025-04-15 17:29:28 +0800

    sitemap plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,47 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import viteCompression from 'vite-plugin-compression';
+import Sitemap from 'vite-plugin-sitemap'
 import path from 'path';
 import { AKATSUKI_CLARA, KANARU_HANON, SAOTOME_GABU } from './src/config/constants.ts';
 
-const vtubers : string[] = [ KANARU_HANON , SAOTOME_GABU, AKATSUKI_CLARA ];
+const vtubers: string[] = [ KANARU_HANON, SAOTOME_GABU, AKATSUKI_CLARA ];
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/hanon-uta/' : '/',
-  plugins: [vue(), viteCompression({
-    algorithm: 'gzip',
-    ext: '.gz',
-    threshold: 10240,
-    deleteOriginFile: false
-  })],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
+    base: process.env.NODE_ENV === 'production' ? '/hanon-uta/' : '/',
+    plugins: [ vue(),
+        viteCompression({
+            algorithm: 'gzip',
+            ext: '.gz',
+            threshold: 10240,
+            deleteOriginFile: false
+        }),
+        Sitemap({
+            hostname: 'https://kevinstrax.github.io',
+            dynamicRoutes: [ '/hanon-uta', '/hanon-uta/#/saotomegabu' ], // Your list of routes
+            exclude: [ // Excluded routes
+                '/',
+                '/google19312be880b2f09b',
+                '/hanon-uta/#/akatsukiclara'
+            ],
+        }) ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src')
+        }
+    },
+    optimizeDeps: {
+        include: [ 'bootstrap' ]
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: (id) =>
+                    id.includes('/data/')
+                        ? vtubers.find((vtuber) => id.includes(`/${ vtuber }/`))?.concat('-data')
+                        : undefined
+            }
+        }
     }
-  },
-  optimizeDeps: {
-    include: ['bootstrap']
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: (id) =>
-            id.includes('/data/')
-                ? vtubers.find((vtuber) => id.includes(`/${vtuber}/`))?.concat('-data')
-                : undefined
-      }
-    }
-  }
 })
