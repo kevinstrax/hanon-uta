62cbf9b4fbb570b2526f3474a79e10f8367a1867
kevinstrax <847967921@qq.com>
2025-04-18 15:39:11 +0800

    Updates

---
+++ assets/Clara-CBGUqPUP.js
@@ -0,0 +1,1 @@
+import{H as e}from"./HelloWorld-Dx5XfyGH.js";import{d as r,c as o,u as a,A as t,o as n}from"./index-DqXeI0lj.js";const p=r({__name:"Clara",setup(s){return(c,l)=>(n(),o(e,{vtuber:a(t)},null,8,["vtuber"]))}});export{p as default};
