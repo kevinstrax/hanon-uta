ca0f42e2cf3da8773c31c770113a667cec69c72d
kevinstrax <847967921@qq.com>
2025-05-04 01:50:28 +0800

    Updates

---
+++ assets/Gabu-CUIojxzM.js
@@ -0,0 +1,1 @@
+import{H as e}from"./HelloWorld-D6kLt5u3.js";import{d as o,c as r,u as a,o as t,V as n}from"./index-BblrFxFo.js";const m=o({__name:"Gabu",setup(s){return(u,c)=>(t(),r(e,{vtuber:a(n).SAOTOME_GABU.name},null,8,["vtuber"]))}});export{m as default};
--- assets/Hanon-C1ob-XYS.js
+++
@@ -1,1 +0,0 @@
-import{H as e}from"./HelloWorld-BkGfI9k5.js";import{d as o,c as r,o as a,u as n,V as t}from"./index-BErYNcuO.js";const m=o({__name:"Hanon",setup(s){return(c,u)=>(a(),r(e,{vtuber:n(t).KANARU_HANON.name},null,8,["vtuber"]))}});export{m as default};
