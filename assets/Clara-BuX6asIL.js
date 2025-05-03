df2067f8992ab4596c2a988a07dcd0f8109e2ebf
kevinstrax <847967921@qq.com>
2025-05-04 01:51:33 +0800

    Updates

---
+++ assets/Clara-BuX6asIL.js
@@ -0,0 +1,1 @@
+import{H as e}from"./HelloWorld-D91Fudw1.js";import{d as r,c as a,u as o,o as t,V as n}from"./index-Cn0JTHqX.js";const m=r({__name:"Clara",setup(s){return(c,l)=>(t(),a(e,{vtuber:o(n).AKATSUKI_CLARA.name},null,8,["vtuber"]))}});export{m as default};
