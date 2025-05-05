c8f559d781e5050e9c7394a53250522c6e254da7
kevinstrax <847967921@qq.com>
2025-05-05 12:46:26 +0800

    Updates

---
+++ assets/Clara-data-BTlZi84F.js
@@ -0,0 +1,36 @@
+const e="✧︎*.〖 歌枠 〗ボカロ縛り💙 プリンボイスとの化学反応なるかっ⁉⋆꙳〖#暁月クララ┊︎#パレプロ〗",t="暁月クララ / Akatsuki Clara【パレプロ】",i="iavuOMwYjpg",o="2025-04-09",n=`＜セトリ＞
+0:08:53 キャットフード/doriko
+0:17:23 おこちゃま戦争/ギガP
+0:24:43 いかないで/想太
+0:28:12 独りんぼエンヴィー/koyori（電ポルP）
+0:35:30 虎視眈々/梅とら
+
+0:41:29 ルカルカ★ナイトフィーバー/samfree
+0:48:18 ロミオとシンデレラ/doriko
+0:56:12 Dear/19's Sound Factory
+1:05:09 深海シティアンダーグラウンド/田中B
+1:11:00 ワールドイズマイン/supercell 
+
+1:17:26 さよならのかわりに/レフティーモンスターP 
+1:28:30 狐ノ嫁入リ/OSTER project
+1:38:57 ピアノ×フォルテ×スキャンダル/OSTER project
+1:46:13 Alice in Musicland/OSTER project
+
+Alice in Musiclandがくーちゃんの歌声で聞けて嬉しかったです
+物語を聞いているようでした！`,b={video_title:e,video_artist:t,video_id:i,video_publish_date_str:o,song_timeline:n},S=Object.freeze(Object.defineProperty({__proto__:null,default:b,song_timeline:n,video_artist:t,video_id:i,video_publish_date_str:o,video_title:e},Symbol.toStringTag,{value:"Module"})),_="✧︎*.〖 #ぽかぽかあったか歌枠リレー ☀〗プリンボイスで幸せ気分をお届けします💗⋆꙳〖#暁月クララ┊︎#パレプロ〗",s="暁月クララ / Akatsuki Clara【パレプロ】",d="5DcGXMCVIk8",l="2025-04-25",r=`＜セトリ＞
+00:02:03 僕らは今のなかで/μ's
+00:09:04 明日も/MUSH&Co. 
+00:13:16 ラムのラブソング/松谷祐子
+00:16:17 Wishing/レム(水瀬いのり)
+00:25:03 光の惑星/Sputrip　※オリジナルソング
+
+Wishingの歌い方すごく良かったです
+オリジナルソング歌ったのビックリしました！`,g={video_title:_,video_artist:s,video_id:d,video_publish_date_str:l,song_timeline:r},k=Object.freeze(Object.defineProperty({__proto__:null,default:g,song_timeline:r,video_artist:s,video_id:d,video_publish_date_str:l,video_title:_},Symbol.toStringTag,{value:"Module"})),a="✧︎*.〖 ＃フレッシュカラー歌枠リレー 〗プリンボイスで心HAPPY✨一緒に楽しもうねっ💗⋆꙳〖#暁月クララ┊︎#パレプロ〗",c="暁月クララ / Akatsuki Clara【パレプロ】",v="Y8wqMykVkbk",u="2025-05-02",p=`＜セトリ＞
+0:02:17 チェリボム/SILENT SIREN
+0:09:33 Booo!/ TOKOTOKO（西沢さんP） 
+0:12:48 シル・ヴ・プレジデント/P丸様。
+0:17:30 カントリーロード/本名陽子
+0:24:17 キミイロクロニクル/PaletteProject　※オリジナル曲
+
+盛り上がる曲いっぱいでとっても楽しくフレッシュでした！
+オリジナル曲も良かったです！`,O={video_title:a,video_artist:c,video_id:v,video_publish_date_str:u,song_timeline:p},$=Object.freeze(Object.defineProperty({__proto__:null,default:O,song_timeline:p,video_artist:c,video_id:v,video_publish_date_str:u,video_title:a},Symbol.toStringTag,{value:"Module"}));export{S as _,k as a,$ as b};
--- assets/Hanon-CMmfDmpK.js
+++
@@ -1,1 +0,0 @@
-import{H as e}from"./HelloWorld-BQHgeJVb.js";import{d as o,C as r,j as a,u as n,E as t}from"./bootstrap.bundle.min-CGEtueWU.js";import"./index-BxrRc_Wu.js";const l=o({__name:"Hanon",setup(s){return(m,p)=>(a(),r(e,{vtuber:n(t).KANARU_HANON.name},null,8,["vtuber"]))}});export{l as default};
