4fa17851f644119e1a539b080e3290d6caf07ce6
kevinstrax <847967921@qq.com>
2025-05-05 17:35:23 +0800

    Updates

--- assets/Hanon-B4_mTm2I.js
+++
@@ -1,1 +0,0 @@
-import{H as e}from"./HelloWorld-C0j22k-1.js";import{d as o,C as r,j as a,u as n,E as t}from"./bootstrap.bundle.min-CGEtueWU.js";import"./index-3LwKOPyJ.js";const l=o({__name:"Hanon",setup(s){return(m,p)=>(a(),r(e,{vtuber:n(t).KANARU_HANON.name},null,8,["vtuber"]))}});export{l as default};
