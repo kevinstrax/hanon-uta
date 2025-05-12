4fbdba5400534c498175c6f811982e5d2fb8f668
kevinstrax <847967921@qq.com>
2025-05-12 12:30:26 +0800

    Updates

---
+++ assets/Clara-4eIOlsxk.js
@@ -0,0 +1,1 @@
+import{H as e}from"./HelloWorld-Cx-vhYhY.js";import{d as r,C as a,u as o,j as t,E as n}from"./bootstrap.bundle.min-BIQM3IOP.js";import"./index-t8RiQNOW.js";const c=r({__name:"Clara",setup(s){return(l,m)=>(t(),a(e,{vtuber:o(n).AKATSUKI_CLARA.name},null,8,["vtuber"]))}});export{c as default};
