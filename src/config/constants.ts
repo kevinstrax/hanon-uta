2c2804377735347e822a6d8aedfdb501e29fa5f3
kevinstrax <847967921@qq.com>
2025-05-15 01:41:17 +0800

    fix title

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,7 +2,7 @@
 export const DEFAULT_PAGE_SIZE: number = 10;
 
 export const SITE_BRAND : string = '香鳴ハノン';
-export const SITE_SUFFIX : string = 'ャンネル歌枠検索（非公式）';
+export const SITE_SUFFIX : string = 'チャンネル歌枠検索（非公式）';
 export const SITE_DESC : string = 'さんの過去の歌枠から、ライブ配信内の楽曲を瞬時に検索。' +
     '曲名やアーティスト名を入力するだけで、該当箇所にワンクリックで移動できる非公式ツール。';
 
