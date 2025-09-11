7c2372c32a088de66194407f77c57ad7b6077669
kevinstrax <847967921@qq.com>
2025-09-12 01:13:40 +0800

    Updates

---
+++ assets/Clara-DZLKhPu9.js
@@ -0,0 +1,1 @@
+import{H as e}from"./HelloWorld-DBw2JzLf.js";import{d as r,D as a,u as o,k as t,G as n}from"./bootstrap.esm-DExPeSsN.js";import"./index-YIodjL82.js";const c=r({__name:"Clara",setup(s){return(l,m)=>(t(),a(e,{vtuber:o(n).AKATSUKI_CLARA.name},null,8,["vtuber"]))}});export{c as default};
--- assets/Hanon-zjWJXJiG.js
+++
@@ -1,1 +0,0 @@
-import{H as e}from"./HelloWorld-B6LdRJgM.js";import{d as o,D as r,k as a,u as n,G as t}from"./bootstrap.esm-DExPeSsN.js";import"./index-TezyFr4d.js";const l=o({__name:"Hanon",setup(s){return(m,p)=>(a(),r(e,{vtuber:n(t).KANARU_HANON.name},null,8,["vtuber"]))}});export{l as default};
