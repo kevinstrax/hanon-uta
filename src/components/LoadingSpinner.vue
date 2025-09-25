50b2f254bea58973b786a1bb782787e288965ced
kevinstrax <847967921@qq.com>
2025-09-25 15:31:32 +0800

    lazy init loading

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useColorModeStore } from "@/stores/color-mode.ts";
+
+const { isDark } = storeToRefs(useColorModeStore())
+</script>
+
+<template>
+  <Transition name="fade">
+    <div
+        class="w-100 h-100 d-flex justify-content-center align-items-center"
+        :class="isDark ? 'bg-dark' : 'bg-white'"
+    >
+      <div class="text-center">
+        <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<style scoped>
+.fade-leave-active {
+  transition: opacity 0.5s ease-out;
+}
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- src/components/SongStatsModal.vue
+++ src/components/SongStatsModal.vue
@@ -3,8 +3,12 @@ import type { Song } from "@/types/song";
 import { storeToRefs } from "pinia";
 import { useStatsInitStore } from "@/stores/stats-init.ts";
 import { defineAsyncComponent } from "vue";
+import LoadingSpinner from "@/components/LoadingSpinner.vue";
 
-const SongStats = defineAsyncComponent(() => import("@/components/SongStats.vue"))
+const SongStats = defineAsyncComponent({
+  loader: () => import("@/components/SongStats.vue"),
+  loadingComponent: LoadingSpinner,
+})
 
 const props = defineProps<{ allSongs: Song[], vtuber: string }>();
 const { isStatsInit } = storeToRefs(useStatsInitStore())
@@ -13,12 +17,13 @@ const { isStatsInit } = storeToRefs(useStatsInitStore())
 <template>
   <div id="exampleModal2" aria-hidden="true" aria-labelledby="exampleModalLabel2" class="modal fade" tabindex="-2">
     <div class="modal-dialog modal-xl modal-dialog-scrollable">
-      <div class="modal-content">
+      <div class="modal-content h-100">
         <div class="modal-header">
-          <h1 id="exampleModalLabel2" class="modal-title fs-5">{{vtuber}}ちゃん歌唱統計</h1>
+          <h1 id="exampleModalLabel2" class="modal-title fs-5">{{props.vtuber}}ちゃん歌唱統計</h1>
           <button aria-label="Close" class="btn-close" data-bs-dismiss="modal" type="button"></button>
         </div>
-        <div class="modal-body">
+        <div class="modal-body position-relative">
+<!--          <LoadingSpinner />-->
           <SongStats v-if="isStatsInit" :all-songs="props.allSongs" :vtuber="props.vtuber" />
         </div>
         <!--        <div class="modal-footer">-->
