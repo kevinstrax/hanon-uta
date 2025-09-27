b278ddc9e9dccd5c73cdf13bf1be1ed213971b7e
kevinstrax <847967921@qq.com>
2025-09-28 00:24:59 +0800

    Updates

--- assets/Gabu-BF-nZoPr.js
+++
@@ -1,1 +0,0 @@
-import{H as e}from"./HelloWorld-DYYpOPOL.js";import{e as r,G as o,u as a,m as t,H as n}from"./bootstrap.esm-C8HwTkrF.js";import"./index-Drz5jYF1.js";const l=r({__name:"Gabu",setup(s){return(m,u)=>(t(),o(e,{vtuber:a(n).SAOTOME_GABU.name},null,8,["vtuber"]))}});export{l as default};
