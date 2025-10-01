58b929c757c3a61e4bb4b82484ab6e9b436f65a2
kevinstrax <847967921@qq.com>
2025-10-01 17:27:59 +0800

    Updates

---
+++ assets/Clara-2019-data-DcWV6KPb.js
@@ -0,0 +1,19 @@
+const e="クララの生歌も受け止めて。",t="暁月クララ / Akatsuki Clara【パレプロ】",o="2n8pfCEHCQs",i="2019-07-12 18:46:07",n=`17:00　1925/冨田悠斗（とみー）
+27:34　深海少女/ゆうゆ
+41:38　Fire◎Flower/halyosy
+53:57　たばこ/コレサワ
+1:06:15　星間飛行/ランカ・リー(中島愛)
+1:14:14　キリトリセン/40mP
+1:20:43　ヒロイン/back number
+1:26:22　U.S.A./DA PUMP
+1:33:36　つけまつける/きゃりーぱみゅぱみゅ
+1:41:25　ポニーテールとシュシュ/AKB48
+1:48:22　サイサイメドレー
+1:59:16　千本桜/黒うさP`,r={video_title:e,video_artist:t,video_id:o,video_publish_date_str:i,song_timeline:n},v=Object.freeze(Object.defineProperty({__proto__:null,default:r,song_timeline:n,video_artist:t,video_id:o,video_publish_date_str:i,video_title:e},Symbol.toStringTag,{value:"Module"})),_="【ヤンデレ注意】歌って喋る配信【愛してる？】",s="暁月クララ / Akatsuki Clara【パレプロ】",a="TPAX7kcB2B8",d="2019-09-10 19:21:30",l=`14:48 結ンデ開イテ羅刹ト骸/ハチ feat. 初音ミク
+25:03 サイノウサンプラー/koyori(電ポルP)
+32:30 アンパンマンのマーチ/ドリーミング
+40:09 東京テディベア/Neru
+47:18 かなしみのなみにおぼれる/Neru
+52:35 季節は次々死んでいく/amazarashi#{=ワンコーラス}
+57:30 バレリーコ/みきとP
+1:16:35 おジャ魔女カーニバル!!/MAHO堂`,c={video_title:_,video_artist:s,video_id:a,video_publish_date_str:d,song_timeline:l},u=Object.freeze(Object.defineProperty({__proto__:null,default:c,song_timeline:l,video_artist:s,video_id:a,video_publish_date_str:d,video_title:_},Symbol.toStringTag,{value:"Module"}));export{v as _,u as a};
--- assets/Clara-C_QVF2tG.js
+++
@@ -1,1 +0,0 @@
-import{H as e}from"./HelloWorld-CFYVo6Im.js";import{e as r,G as a,u as o,m as t,H as n}from"./bootstrap.esm-2XW23Nch.js";import"./index-B18k1Iz2.js";const c=r({__name:"Clara",setup(s){return(m,l)=>(t(),a(e,{vtuber:o(n).AKATSUKI_CLARA.name},null,8,["vtuber"]))}});export{c as default};
