43fc8f525aea14490b985abeed53af9037182437
kevinstrax <847967921@qq.com>
2025-10-04 18:42:24 +0800

    redirect to hanon-uta.github.io

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,11 @@ import { use404Redirect } from "@/utils/404Redirect";
 import { useColorModeStore } from "@/stores/color-mode.ts";
 import { onBeforeUnmount, onMounted } from "vue";
 
+const newBase = "https://hanon-uta.github.io";
+const oldPath = window.location.pathname.replace(/^\/hanon-uta/, "");
+const newUrl = newBase + oldPath + window.location.search + window.location.hash;
+window.location.replace(newUrl);
+
 const loadingStore = useLoadingStore()
 const { isInitialLoad } = storeToRefs(loadingStore)
 
